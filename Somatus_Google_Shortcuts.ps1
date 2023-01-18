1801e8c1f08115ba345d1769fd36167b15280873
atsjason <98031074+atsjason@users.noreply.github.com>
2023-01-18 17:01:44 -0500

    Update Somatus_Google_Shortcuts.ps1

--- Somatus_Google_Shortcuts.ps1
+++ Somatus_Google_Shortcuts.ps1
@@ -2,6 +2,18 @@ $Path = 'C:\Shortcuts'
     if (!(Test-Path -Path 'C:\Shortcuts'))
         {New-Item -Path $Path -ItemType Directory}
 
+$TBX_URL = "https://thebenefitsexpert.com/favicon.ico"
+$TBX_ICO = Join-Path -Path $Path -ChildPath "\TBX.ico"
+(New-Object System.Net.WebClient).DownloadFile("${TBX_URL}", "${TBX_ICO}")
+
+$UKG_URL = "https://ew13.ultipro.com/favicon.ico"
+$UKG_ICO = Join-Path -Path $Path -ChildPath "\UKG.ico"
+(New-Object System.Net.WebClient).DownloadFile("${UKG_URL}", "${UKG_ICO}")
+
+$NHO_URL = "https://www.microsoft.com/favicon.ico"
+$NHO_ICO = Join-Path -Path $Path -ChildPath "\SharePoint.ico"
+(New-Object System.Net.WebClient).DownloadFile("${NHO_URL}", "${NHO_ICO}")
+
 
 if ((Test-Path -Path "C:\Program Files\Google\Chrome\Application")){
 $WshShell = New-Object -comObject WScript.Shell
@@ -11,8 +23,9 @@ $DestinationPath = Join-Path -Path $destination -ChildPath "\\TBX.lnk"
 $SourceExe = "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe"
 $Shortcut = $WshShell.CreateShortcut($DestinationPath)
 $Shortcut.TargetPath = $SourceExe
-$Shortcut.arguments = "--app=https://thebenefitsexpert.com/"
-$shortcut.IconLocation = "https://thebenefitsexpert.com/favicon.ico"
+$Shortcut.arguments = "--app=https://thebenefitsexpert.com/enroll/login.aspx?ts=637889835040032787&Path=somatus/"
+#$shortcut.IconLocation = "https://thebenefitsexpert.com/favicon.ico"
+$shortcut.IconLocation = "C:\Shortcuts\TBX.ico"
 $Shortcut.Save()
 }
 
@@ -25,7 +38,8 @@ $SourceExe = "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe"
 $Shortcut = $WshShell.CreateShortcut($DestinationPath)
 $Shortcut.TargetPath = $SourceExe
 $Shortcut.arguments = "--app=https://ew13.ultipro.com/"
-$shortcut.IconLocation = "https://ew13.ultipro.com/favicon.ico"
+#$shortcut.IconLocation = "https://ew13.ultipro.com/favicon.ico"
+$shortcut.IconLocation = "C:\Shortcuts\UKG.ico"
 $Shortcut.Save()
 }
 
@@ -38,7 +52,8 @@ $SourceExe = "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe"
 $Shortcut = $WshShell.CreateShortcut($DestinationPath)
 $Shortcut.TargetPath = $SourceExe
 $Shortcut.arguments = "--app=https://somatusoffice365.sharepoint.com/sites/NewHireOrientation"
-$shortcut.IconLocation = "https://www.microsoft.com/favicon.ico"
+#$shortcut.IconLocation = "https://www.microsoft.com/favicon.ico"
+$shortcut.IconLocation = "C:\Shortcuts\SharePoint.ico"
 $Shortcut.Save()
 }
 
@@ -50,8 +65,9 @@ $DestinationPath = Join-Path -Path $destination -ChildPath "\\TBX.lnk"
 $SourceExe = "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe"
 $Shortcut = $WshShell.CreateShortcut($DestinationPath)
 $Shortcut.TargetPath = $SourceExe
-$Shortcut.arguments = "--app=https://thebenefitsexpert.com/"
-$shortcut.IconLocation = "https://thebenefitsexpert.com/favicon.ico"
+$Shortcut.arguments = "--app=https://thebenefitsexpert.com/enroll/login.aspx?ts=637889835040032787&Path=somatus"
+#$shortcut.IconLocation = "https://thebenefitsexpert.com/favicon.ico"
+$shortcut.IconLocation = "C:\Shortcuts\TBX.ico"
 $Shortcut.Save()
 }
 
@@ -64,7 +80,8 @@ $SourceExe = "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe"
 $Shortcut = $WshShell.CreateShortcut($DestinationPath)
 $Shortcut.TargetPath = $SourceExe
 $Shortcut.arguments = "--app=https://ew13.ultipro.com/"
-$shortcut.IconLocation = "https://ew13.ultipro.com/favicon.ico"
+#$shortcut.IconLocation = "https://ew13.ultipro.com/favicon.ico"
+$shortcut.IconLocation = "C:\Shortcuts\UKG.ico"
 $Shortcut.Save()
 }
 
@@ -77,7 +94,8 @@ $SourceExe = "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe"
 $Shortcut = $WshShell.CreateShortcut($DestinationPath)
 $Shortcut.TargetPath = $SourceExe
 $Shortcut.arguments = "--app=https://somatusoffice365.sharepoint.com/sites/NewHireOrientation"
-$shortcut.IconLocation = "https://www.microsoft.com/favicon.ico"
+#$shortcut.IconLocation = "https://www.microsoft.com/favicon.ico"
+$shortcut.IconLocation = "C:\Shortcuts\SharePoint.ico"
 $Shortcut.Save()
 }
 
